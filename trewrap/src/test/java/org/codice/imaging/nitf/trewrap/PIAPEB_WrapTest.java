6edc17a4bcd46628afd9c590fd3807f25a008b2c
john.main <john.main.4@us.af.mil>
2019-07-09 14:37:07 -0400

    * updated PIAPEB_WrapTest to remove a potential null pointer exception

--- trewrap/src/test/java/org/codice/imaging/nitf/trewrap/PIAPEB_WrapTest.java
+++ trewrap/src/test/java/org/codice/imaging/nitf/trewrap/PIAPEB_WrapTest.java
@@ -36,125 +36,139 @@ import uk.org.lidalia.slf4jtest.TestLoggerFactory;
  */
 public class PIAPEB_WrapTest extends SharedTreTestSupport {
 
-    TestLogger LOGGER = TestLoggerFactory.getTestLogger(TreWrapper.class);
+	TestLogger LOGGER = TestLoggerFactory.getTestLogger(TreWrapper.class);
 
-    private final String mTestData = "PIAPEB00094Maxwell                     James                       Clerk                       18310613UK";
-    private final String mAllSpaces = "PIAPEB00094                                                                                              ";
-    
-    public PIAPEB_WrapTest() {
-    }
+	private final String mTestData = "PIAPEB00094Maxwell                     James                       Clerk                       18310613UK";
+	private final String mAllSpaces = "PIAPEB00094                                                                                              ";
 
-    @Test
-    public void testGetters() throws NitfFormatException {
-        Tre tre = parseTRE(mTestData, "PIAPEB");
-        PIAPEB piapeb = new PIAPEB(tre);
-        assertEquals("Maxwell", piapeb.getLastName());
-        assertEquals("James", piapeb.getFirstName());
-        assertEquals("Clerk", piapeb.getMiddleName());
-        assertEquals(LocalDate.of(1831, 6, 13), piapeb.getBirthDate());
-        assertEquals("UK", piapeb.getAssociatedCountryCode());
-    }
-    
-    @Test
-    public void testGettersAllSpaces() throws NitfFormatException {
-        Tre tre = parseTRE(mAllSpaces, "PIAPEB");
-        PIAPEB piapeb = new PIAPEB(tre);
-        assertEquals("", piapeb.getLastName());
-        assertEquals("", piapeb.getFirstName());
-        assertEquals("", piapeb.getMiddleName());
-        assertThat(LOGGER.getLoggingEvents().isEmpty(), is(true));
-        assertEquals(null, piapeb.getBirthDate());
-        assertThat(LOGGER.getLoggingEvents(), is(Arrays.asList(LoggingEvent.debug("Could not parse          as a local date: Text '        ' could not be parsed at index 0"))));
-        assertEquals("  ", piapeb.getAssociatedCountryCode());
-    }
+	public PIAPEB_WrapTest() {
+	}
 
-    @Rule
-    public ExpectedException exception = ExpectedException.none();
+	@Test
+	public void testGetters() throws NitfFormatException {
+		Tre tre = parseTRE(mTestData, "PIAPEB");
+		PIAPEB piapeb = new PIAPEB(tre);
+		assertEquals("Maxwell", piapeb.getLastName());
+		assertEquals("James", piapeb.getFirstName());
+		assertEquals("Clerk", piapeb.getMiddleName());
+		assertEquals(LocalDate.of(1831, 6, 13), piapeb.getBirthDate());
+		assertEquals("UK", piapeb.getAssociatedCountryCode());
+	}
 
-    @Test
-    public void checkBadUsage() throws NitfFormatException {
-        Tre tre = TreFactory.getDefault("PIAEVA", TreSource.ImageExtendedSubheaderData);
-        exception.expect(IllegalStateException.class);
-        exception.expectMessage("Incorrect TRE name for PIAPEB wrapper");
-        PIAPEB piapeb = new PIAPEB(tre);
-    }
+	@Test
+	public void testGettersAllSpaces() throws NitfFormatException {
+		Tre tre = parseTRE(mAllSpaces, "PIAPEB");
+		PIAPEB piapeb = new PIAPEB(tre);
+		if (piapeb == null) {
+			assertFail("PIAPEB is null");
+		} else {
+			assertEquals("", piapeb.getLastName());
+			assertEquals("", piapeb.getFirstName());
+			assertEquals("", piapeb.getMiddleName());
+			assertThat(LOGGER.getLoggingEvents().isEmpty(), is(true));
+			assertEquals(null, piapeb.getBirthDate());
+			assertThat(LOGGER.getLoggingEvents(), is(Arrays.asList(LoggingEvent.debug(
+					"Could not parse as a local date: Text '        ' could not be parsed at index 0"))));
+			assertEquals("  ", piapeb.getAssociatedCountryCode());
+		}
+	}
 
-    @Test
-    public void checkBuildOrder() throws NitfFormatException {
-        PIAPEB piapeb = new PIAPEB(TreSource.ImageExtendedSubheaderData);
-        piapeb.setLastName("AB");
-        piapeb.setBirthDate(LocalDate.of(1987, 3, 15));
-        piapeb.setMiddleName("CD");
-        piapeb.setFirstName("EF");
-        piapeb.setAssociatedCountry("US");
-        assertEquals("AB", piapeb.getLastName());
-        piapeb.setLastName("ZY");
-        assertEquals("ZY", piapeb.getLastName());
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
 
-        Tre tre = piapeb.getTRE();
-        assertNotNull(tre);
-        assertEquals("ZY", tre.getFieldValue("LASTNME"));
+	@Test
+	public void checkBadUsage() throws NitfFormatException {
+		Tre tre = TreFactory.getDefault("PIAEVA", TreSource.ImageExtendedSubheaderData);
+		exception.expect(IllegalStateException.class);
+		exception.expectMessage("Incorrect TRE name for PIAPEB wrapper");
+		PIAPEB piapeb = new PIAPEB(tre);
+	}
 
-        byte[] serialisedTRE = piapeb.serialize();
-        Assert.assertArrayEquals("ZY                          EF                          CD                          19870315US".getBytes(), serialisedTRE);
-    }
+	@Test
+	public void checkBuildOrder() throws NitfFormatException {
+		PIAPEB piapeb = new PIAPEB(TreSource.ImageExtendedSubheaderData);
+		piapeb.setLastName("AB");
+		piapeb.setBirthDate(LocalDate.of(1987, 3, 15));
+		piapeb.setMiddleName("CD");
+		piapeb.setFirstName("EF");
+		piapeb.setAssociatedCountry("US");
+		assertEquals("AB", piapeb.getLastName());
+		piapeb.setLastName("ZY");
+		assertEquals("ZY", piapeb.getLastName());
 
-    @Test
-    public void checkBuildWithOnlyDefaults() throws NitfFormatException {
-        PIAPEB piapeb = new PIAPEB(TreSource.UserDefinedImageData);
-        byte[] serialisedTRE = piapeb.serialize();
-        Assert.assertArrayEquals("                                                                                              ".getBytes(), serialisedTRE);
-    }
-    
-    @Test
-    public void checkBuildOrderPartialValues() throws NitfFormatException {
-        PIAPEB piapeb = new PIAPEB(TreSource.ImageExtendedSubheaderData);
-        piapeb.setBirthDate(LocalDate.of(1987, 3, 15));
-        piapeb.setLastName("ZY");
-        byte[] serialisedTRE = piapeb.serialize();
-        Assert.assertArrayEquals("ZY                                                                                  19870315  ".getBytes(), serialisedTRE);
-    }
+		Tre tre = piapeb.getTRE();
+		assertNotNull(tre);
+		assertEquals("ZY", tre.getFieldValue("LASTNME"));
 
-    @Test
-    public void checkBuild_IsValid() throws NitfFormatException {
-        PIAPEB piapeb = new PIAPEB(TreSource.ExtendedHeaderData);
-        assertTrue(piapeb.getValidity().isValid());
-    }
+		byte[] serialisedTRE = piapeb.serialize();
+		Assert.assertArrayEquals(
+				"ZY                          EF                          CD                          19870315US"
+						.getBytes(),
+				serialisedTRE);
+	}
 
-    @Test
-    public void checkBuild_IsValidNullLastNameValue() throws NitfFormatException {
-        PIAPEB piapeb = new PIAPEB(TreSource.ImageExtendedSubheaderData);
-        piapeb.setLastName(null);
-        assertFalse(piapeb.getValidity().isValid());
-        assertEquals("LASTNME may not be null", piapeb.getValidity().getValidityResultDescription());
-    }
+	@Test
+	public void checkBuildWithOnlyDefaults() throws NitfFormatException {
+		PIAPEB piapeb = new PIAPEB(TreSource.UserDefinedImageData);
+		byte[] serialisedTRE = piapeb.serialize();
+		Assert.assertArrayEquals(
+				"                                                                                              "
+						.getBytes(),
+				serialisedTRE);
+	}
 
-    @Test
-    public void checkBuild_IsValidNullFirstNameValue() throws NitfFormatException {
-        PIAPEB piapeb = new PIAPEB(TreSource.UserDefinedImageData);
-        piapeb.setFirstName(null);
-        assertFalse(piapeb.getValidity().isValid());
-        assertEquals("FIRSTNME may not be null", piapeb.getValidity().getValidityResultDescription());
-    }
+	@Test
+	public void checkBuildOrderPartialValues() throws NitfFormatException {
+		PIAPEB piapeb = new PIAPEB(TreSource.ImageExtendedSubheaderData);
+		piapeb.setBirthDate(LocalDate.of(1987, 3, 15));
+		piapeb.setLastName("ZY");
+		byte[] serialisedTRE = piapeb.serialize();
+		Assert.assertArrayEquals(
+				"ZY                                                                                  19870315  "
+						.getBytes(),
+				serialisedTRE);
+	}
 
-    @Test
-    public void checkBuild_IsValidNullMiddleNameValue() throws NitfFormatException {
-        PIAPEB piapeb = new PIAPEB(TreSource.UserDefinedImageData);
-        piapeb.setMiddleName(null);
-        assertFalse(piapeb.getValidity().isValid());
-        assertEquals("MIDNME may not be null", piapeb.getValidity().getValidityResultDescription());
-    }
+	@Test
+	public void checkBuild_IsValid() throws NitfFormatException {
+		PIAPEB piapeb = new PIAPEB(TreSource.ExtendedHeaderData);
+		assertTrue(piapeb.getValidity().isValid());
+	}
 
-    @Test
-    public void checkBuild_IsValidNullAssociatedCountryValue() throws NitfFormatException {
-        PIAPEB piapeb = new PIAPEB(TreSource.ImageExtendedSubheaderData);
-        piapeb.setAssociatedCountry(null);
-        assertFalse(piapeb.getValidity().isValid());
-        assertEquals("ASSOCTRY may not be null", piapeb.getValidity().getValidityResultDescription());
-    }
+	@Test
+	public void checkBuild_IsValidNullLastNameValue() throws NitfFormatException {
+		PIAPEB piapeb = new PIAPEB(TreSource.ImageExtendedSubheaderData);
+		piapeb.setLastName(null);
+		assertFalse(piapeb.getValidity().isValid());
+		assertEquals("LASTNME may not be null", piapeb.getValidity().getValidityResultDescription());
+	}
 
-    @Before
-    public void clearLoggers() {
-        TestLoggerFactory.clear();
-    }
+	@Test
+	public void checkBuild_IsValidNullFirstNameValue() throws NitfFormatException {
+		PIAPEB piapeb = new PIAPEB(TreSource.UserDefinedImageData);
+		piapeb.setFirstName(null);
+		assertFalse(piapeb.getValidity().isValid());
+		assertEquals("FIRSTNME may not be null", piapeb.getValidity().getValidityResultDescription());
+	}
+
+	@Test
+	public void checkBuild_IsValidNullMiddleNameValue() throws NitfFormatException {
+		PIAPEB piapeb = new PIAPEB(TreSource.UserDefinedImageData);
+		piapeb.setMiddleName(null);
+		assertFalse(piapeb.getValidity().isValid());
+		assertEquals("MIDNME may not be null", piapeb.getValidity().getValidityResultDescription());
+	}
+
+	@Test
+	public void checkBuild_IsValidNullAssociatedCountryValue() throws NitfFormatException {
+		PIAPEB piapeb = new PIAPEB(TreSource.ImageExtendedSubheaderData);
+		piapeb.setAssociatedCountry(null);
+		assertFalse(piapeb.getValidity().isValid());
+		assertEquals("ASSOCTRY may not be null", piapeb.getValidity().getValidityResultDescription());
+	}
+
+	@Before
+	public void clearLoggers() {
+		TestLoggerFactory.clear();
+	}
 }
